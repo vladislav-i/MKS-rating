5e3b69072940f440b08405b9f83b07dc47f64099
Bryan Lee <pygmize.me@gmail.com>
2015-06-01 17:48:00 -0700

    [feature] Change companyView to listen to changes in model

--- Client/javascript/views/companyView.js
+++ Client/javascript/views/companyView.js
@@ -1,8 +1,9 @@
 var companyView = Backbone.View.extend({
   initialize: function(){
     this.render();
+    this.listenTo(this.model, 'change', this.render);
   },
   render: function(){
-    this.$el.html('<div>' + this.model.get('company').toString() + '</div>');
+    this.$el.html('<div>' + this.model.get('name') + ',' + this.model.get('location') + '</div>');
   }
 });
